04c88867b8458b2720498ccd615bdff9dd201462
Sameer Dhole <dholesameer202@gmail.com>
2021-10-03 12:27:00 +0530

    kl div

--- network/loss_functions.py
+++ network/loss_functions.py
@@ -16,3 +16,11 @@ def huber_loss(X, Y):
 def mse_loss(X, Y):
     err = X - Y
     return tf.reduce_sum(tf.square(err))
+
+def kl_loss(y_true,y_pred):
+    y_true = tf.clip_by_value(y_true, 0, 1)
+    y_pred = tf.clip_by_value(y_pred, 0, 1)
+    return tf.reduce_sum(y_true * tf.log(y_true / y_pred))
+
+
+
